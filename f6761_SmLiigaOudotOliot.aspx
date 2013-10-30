2ca97c4ac5cd5163623470f367962fc8746de1ae
joelkarttunen <kindjal91@hotmail.com>
2013-10-30 19:44:50 +0200

    Oudot oliot SM-liigaan
    Ei kyllä aivan kaikki rajaukset toimi

---
+++ f6761_SmLiigaOudotOliot.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/f6761_MasterExample.master" AutoEventWireup="true" CodeFile="f6761_SmLiigaOudotOliot.aspx.cs" Inherits="f6761_SmLiigaOudotOliot" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Oudot oliot Sm-liigassa</h1>
+
+    <asp:Label ID="label2" runat="server" Text="Luo uusi pelaaja"></asp:Label> <br />
+    <asp:Label ID="Label3" runat="server" Text="Sukunimi"></asp:Label>
+    <asp:TextBox ID="txtSukunimi" runat="server"></asp:TextBox> <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtSukunimi" ErrorMessage="Nimi vaaditaan" ForeColor="Red" > </asp:RequiredFieldValidator> <br /> 
+    <asp:Label ID="Label4" runat="server" Text="Etunimi"></asp:Label> 
+    <asp:TextBox ID="txtEtunimi" runat="server"></asp:TextBox><asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtEtunimi" ErrorMessage="Nimi vaaditaan" ForeColor="Red" > </asp:RequiredFieldValidator> <br /> 
+    <asp:Label ID="Label5" runat="server" Text="Seura"></asp:Label>
+    <asp:DropDownList ID="listSeura" runat="server"> 
+    </asp:DropDownList><asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="listSeura" ErrorMessage="Seura vaaditaan" ForeColor="Red" > </asp:RequiredFieldValidator> <br /> 
+    <asp:Label ID="Label6" runat="server" Text="Pelipaikka"></asp:Label>
+    <asp:DropDownList ID="listPaikka" runat="server">
+    </asp:DropDownList> <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="listPaikka" ErrorMessage="Pelipaikka vaaditaan" ForeColor="Red" > </asp:RequiredFieldValidator> <br /> 
+    <br />
+    <asp:Button ID="BtnCreatePlayer" runat="server" Text="Tee uusi pelaaja" OnClick="BtnCreatePlayer_Click" /> <br /><br /><br />
+    <asp:Label runat="server">Valitse filttereitä</asp:Label> 
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <br />
+
+    <asp:Button ID="btnAllPlayers" runat="server" Text="Listaa kaikki pelaajat" OnClick="btnAllPlayers_Click" CausesValidation="false" />
+
+    <asp:DropDownList ID="listTeam" runat="server" EnableViewState="true" OnSelectedIndexChanged="listTeam_SelectedIndexChanged" AutoPostBack="true" CausesValidation="false">
+    </asp:DropDownList>
+    <asp:DropDownList ID="listPosition" runat="server" EnableViewState="true" AutoPostBack="true" OnSelectedIndexChanged="listPosition_SelectedIndexChanged " CausesValidation="false">
+    </asp:DropDownList>
+    <br />
+    <asp:Button ID="btnSortByName" runat="server" Text="Järjestä nimen mukaan" OnClick="btnSortByName_Click" CausesValidation="false"/>
+    <asp:Button ID="btnSortByPoints" runat="server" Text="Järjestä pisteiden mukaan" OnClick="btnSortByPoints_Click" CausesValidation="false"/>
+    
+    <asp:GridView ID="GridView1" runat="server" EnableViewState="true">
+    </asp:GridView> <br /><br />
+
+    
+
+
+</asp:Content>
+
---
+++ f6761_SmLiigaOudotOliot.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+
+public partial class f6761_SmLiigaOudotOliot : System.Web.UI.Page
+{
+    /***
+     *  Ei aivan kaikki filtteröinnit toimi
+     * muuten kyllä pitäisi toimia
+     * 
+     * 
+     * */
+    public DataTable dt = new DataTable();
+    public DataView dv = new DataView();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string sFilePath = Server.MapPath(@"App_Data\SMLiiga.mdb");
+
+
+        string conString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + sFilePath;
+        OleDbConnection Conn = new OleDbConnection(conString);
+        dt = new DataTable();
+        dt.TableName = "taabeli";
+        using (Conn)
+        {
+            Conn.Open();
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM Pisteet", Conn);
+            OleDbDataAdapter oDA = new OleDbDataAdapter(cmd);
+
+            oDA.Fill(dt);
+
+        }
+
+        DataView dv = new DataView();
+        dv.Table = dt;
+        dv.Sort = "sukunimi ASC";
+
+        GridView1.DataSource = dv;
+        GridView1.DataBind();
+
+        if (!Page.IsPostBack)
+        {
+            List<String> pelipaikat = new List<String>();
+            pelipaikat.Add("");
+            pelipaikat.Add("P");
+            pelipaikat.Add("H");
+            pelipaikat.Add("M");
+
+            listPosition.DataSource = pelipaikat;
+            listPosition.DataBind();
+
+            listPaikka.DataSource = pelipaikat;
+            listPaikka.DataBind();
+
+            List<String> joukkueet = new List<String>();
+            joukkueet.Add("");
+            joukkueet.Add("Kärpät");
+            joukkueet.Add("Tappara");
+            joukkueet.Add("Lukko");
+            joukkueet.Add("TPS");
+            joukkueet.Add("HIFK");
+            joukkueet.Add("Blues");
+            joukkueet.Add("HPK");
+            joukkueet.Add("Ilves");
+            joukkueet.Add("Kalpa");
+            joukkueet.Add("JYP");
+            joukkueet.Add("Ässät");
+            joukkueet.Add("Pelicans");
+            joukkueet.Add("SaiPa");
+
+            listTeam.DataSource = joukkueet;
+            listTeam.DataBind();
+
+            listSeura.DataSource = joukkueet;
+            listSeura.DataBind();
+
+        }
+
+
+
+    }
+
+    public void IniMyStuff()
+    {
+
+    }
+    protected void btnAllPlayers_Click(object sender, EventArgs e)
+    {
+        dv = (DataView)GridView1.DataSource;
+        dv.Sort = "sukunimi ASC";
+
+        GridView1.DataSource = dv;
+        GridView1.DataBind();
+    }
+    protected void btnSortByName_Click(object sender, EventArgs e)
+    {
+        dv = (DataView)GridView1.DataSource;
+
+        if (listTeam.SelectedItem.ToString() == "" || listPosition.SelectedItem.ToString() == "")
+        {
+
+            dv.Sort = "sukunimi ASC";
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+
+        }
+        else
+        {
+            if (listPosition.SelectedItem.ToString() != "")
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "' AND pelipaikka = " + "'" + listPosition.SelectedItem.ToString() + "'";
+            }
+            else
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+                Label1.Text = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+
+            }
+
+            dv.RowStateFilter = DataViewRowState.CurrentRows;
+            dv.Sort = "sukunimi ASC";
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+        }
+
+    }
+    protected void btnSortByPoints_Click(object sender, EventArgs e)
+    {
+        dv = (DataView)GridView1.DataSource;
+        if (listTeam.SelectedItem.ToString() == "" || listPosition.SelectedItem.ToString() == "")
+        {
+            dv.Sort = "pisteet DESC";
+
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+        }
+        else
+        {
+            if (listPosition.SelectedItem.ToString() != "")
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "' AND pelipaikka = " + "'" + listPosition.SelectedItem.ToString() + "'";
+            }
+            else
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+                Label1.Text = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+
+            }
+            dv.RowStateFilter = DataViewRowState.CurrentRows;
+            dv.Sort = "pisteet DESC";
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+        }
+    }
+    protected void listTeam_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        if (listTeam.SelectedItem.ToString() != "")
+        {
+            dv = (DataView)GridView1.DataSource;
+
+            if (listPosition.SelectedItem.ToString() != "")
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "' AND pelipaikka = " + "'" + listPosition.SelectedItem.ToString() + "'";
+            }
+            else
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+                Label1.Text = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+
+            }
+            dv.RowStateFilter = DataViewRowState.CurrentRows;
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+        }
+
+    }
+    protected void listPosition_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (listPosition.SelectedItem.ToString() != "")
+        {
+            dv = (DataView)GridView1.DataSource;
+
+            if (listTeam.SelectedItem.ToString() != "")
+            {
+                dv.RowFilter = "seura = " + "'" + listTeam.SelectedItem.ToString() + "' AND pelipaikka = " + "'" + listPosition.SelectedItem.ToString() + "'";
+            }
+            else
+            {
+                dv.RowFilter = "pelipaikka = " + "'" + listPosition.SelectedItem.ToString() + "'";
+                Label1.Text = "seura = " + "'" + listTeam.SelectedItem.ToString() + "'";
+
+            }
+            dv.RowStateFilter = DataViewRowState.CurrentRows;
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+        }
+    }
+    protected void BtnCreatePlayer_Click(object sender, EventArgs e)
+    {
+        string sFilePath = Server.MapPath(@"App_Data\SMLiiga.mdb");
+
+
+        string conString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + sFilePath;
+        OleDbConnection Conn = new OleDbConnection(conString);
+
+        using (Conn)
+        {
+            Conn.Open();
+
+            OleDbCommand cmd = new OleDbCommand(String.Format("INSERT INTO Pisteet (id,sukunimi,etunimi,seura,pelipaikka) VALUES('{0}','{1}','{2}','{3}','{4}')",dt.Rows.Count, txtSukunimi.Text, txtEtunimi.Text, listSeura.SelectedItem.ToString(), listPaikka.SelectedItem.ToString()), Conn);
+
+            cmd.ExecuteNonQuery();
+            
+        }
+    }
+}
